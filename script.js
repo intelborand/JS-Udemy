1c3409e409ca72fb6b5b220f1d2dc5fc50809d6d
Andrzej <intelborand@gmail.com>
2020-12-12 18:09:30 +0100

    While and Do

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
 
@@ -13,12 +13,62 @@ const personalMovieDB = {
 };
 
 
-const a = prompt ('Один мз последних просмотренных фильмов', '');
-const b = prompt ('На сколько оцените его?', '');
-const c = prompt ('Один мз последних просмотренных фильмов', '');
-const d = prompt ('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt ('Один мз последних просмотренных фильмов', '');
+    const b = prompt ('На сколько оцените его?', '');
+
+    if (a != null && b != null && a !="" && b !="" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
+
+/*
+let i = 0;
+while (i < 2) {
+    i++
+    const a = prompt ('Один мз последних просмотренных фильмов', '');
+    const b = prompt ('На сколько оцените его?', '');
+    if (a != null && b != null && a !="" && b !="" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
+*/
+
+
+/*
+let i = 0;
+do {
+    i++
+    const a = prompt ('Один мз последних просмотренных фильмов', '');
+    const b = prompt ('На сколько оцените его?', '');
+    if (a != null && b != null && a !="" && b !="" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
+while (i < 2);
+*/
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >=30) {
+    console.log('Вы классический киноман');
+} else {
+    console.log('Error');
+}
 
 console.log(personalMovieDB);
